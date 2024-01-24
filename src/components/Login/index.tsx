efac0f0817d29fe70b8dc5b478050d8ca37d1fa1
ClicianDev <ceedy12345@gmail.com>
2024-01-24 14:42:10 +0800

    update on login player webpage communication to UE5 added

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,52 +1,81 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, FormEvent, useEffect } from 'react';
+import { PixelStreaming } from '@epicgames-ps/lib-pixelstreamingfrontend-ue5.2';
 
 function Login() {
-  // State to manage form input values
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+    const [pixelStreaming, setPixelStreaming] = useState<PixelStreaming>();
 
-  // Function to handle form submission
-  const handleLogin = (e: FormEvent) => {
-    e.preventDefault();
-    // Add logic here to handle login, such as making an API request
-    console.log('Login button clicked!');
-    console.log('Username:', username);
-    console.log('Password:', password);
-    const descriptor = {
-		LoadLevel: "/Game/Maps/Level_2",
-		PlayerCharacter: {
-			Name: "Shinobi",
-			Skin: "Dynasty",
-		},
-	};
-	this.stream.emitUIInteraction(descriptor);
-  };
+    // State to manage form input values
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  return (
-    <div className='bg-black'>
-      <form onSubmit={handleLogin}>
-        <label>
-          Username:
-          <input
-            type='text'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <br />
-        <button type='submit'>Login</button>
-      </form>
-    </div>
-  );
+    // Function to handle form submission
+    const handleLogin = async (e: FormEvent) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch(
+                'http://192.168.68.116:5050/users/login',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ email, password })
+                }
+            );
+
+            if (!response.ok) {
+                const message = `An error occurred: ${response.statusText}`;
+                window.alert(message);
+                return;
+            }
+            const result = await response.json();
+
+            if (result.message === 'Login successful') {
+                window.alert(result.message)
+                const descriptor = {
+                    PlayerCredentials: {
+                        FirstName: result.firstName,
+                        LastName: result.lastName
+                    }
+                };
+                useEffect(() => {
+                    pixelStreaming.emitUIInteraction(descriptor);
+                },[descriptor]);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+        console.log('Login button clicked!');
+        console.log('email:', email);
+        console.log('Password:', password);
+    };
+
+    return (
+        <div className="bg-black">
+            <form onSubmit={handleLogin}>
+                <label>
+                    email:
+                    <input
+                        type="text"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                <br />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
 }
 
 export default Login;
